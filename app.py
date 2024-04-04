758628026366d3cecda36057b7b363163874e0ea
aswinpradeepc <aswinpradeepc@gmail.com>
2024-04-04 22:13:43 +0530

    image resize and req.txt

--- app.py
+++ app.py
@@ -19,15 +19,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Define the endpoint for making predictions
+
+def preprocess_image(img):
+    # Resize to 300x70 pixels
+    img_resized = cv2.resize(img, (300, 70))
+    return img_resized
+
+
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
     # Read the uploaded image file
     contents = await file.read()
-    
-    img = tf.keras.utils.load_img(io.BytesIO(contents), target_size=(70, 300))
-    img_array = tf.keras.utils.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+    # Decode the image using OpenCV
+    img = cv2.imdecode(np.frombuffer(contents, np.uint8), -1)
+
+    # Preprocess the image
+    img_preprocessed = preprocess_image(img)
+
+    # Convert to a format compatible with TensorFlow
+    img_array = tf.keras.preprocessing.image.img_to_array(img_preprocessed)
+    img_array = np.expand_dims(img_array, axis=0)  # Create a batch
 
     # Make predictions using the loaded model
     predictions = model.predict(img_array)
@@ -35,10 +47,11 @@ async def predict(file: UploadFile = File(...)):
 
     predicted_class = np.argmax(score)  # Get the predicted class index
     confidence = 100 * np.max(score)  # Calculate confidence
-    
+
     class_names = ['esotropia', 'exotropia', 'hypertropia', 'hypotropia', 'normal']  # Update with your class names
-    
-    print("This image most likely belongs to {} with a {:.2f}% confidence.".format(class_names[predicted_class], confidence))
-    
+
+    print("This image most likely belongs to {} with a {:.2f}% confidence.".format(class_names[predicted_class],
+                                                                                   confidence))
+
     # Return the prediction result
     return {"class": class_names[predicted_class], "confidence": confidence}
